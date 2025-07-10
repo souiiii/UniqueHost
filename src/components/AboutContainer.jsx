b8b71b88b26f9d398c0d0ef80b61eb673596510f
souiiii <b423037@iiit-bh.ac.in>
2025-07-10 15:03:04 +0530

    navbar for phone is implemented successfully

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -58,6 +58,7 @@ export default function AboutContainer() {
             Discord→
           </a>
         </button>
+        <div className={styles.aboutLoveDiv}><img className={styles.aboutLove} src="/aboutlove.png"/></div>
         <p className={styles.note}>
           Have questions or need support?{" "}
           <span className={styles.hosting}>Reach out</span>! Our support team is
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Menu.module.css';
 
 export default function Menu() {
-  return <div className={styles.parent}>hello</div>;
+  return (
+    <div className={styles.parent}>
+      <div className={styles.container}>
+        <NavLink to="/" className={styles.links}>
+          Home<div className={styles.underline}></div>
+        </NavLink>
+        <NavLink to="/hostings" className={styles.links}>
+          Hosting Plans<div className={styles.underline}></div>
+        </NavLink>
+        <NavLink to="/website-builder" className={styles.links}>
+          Website Builder<div className={styles.underline}></div>
+        </NavLink>
+        <NavLink to="/about-us" className={styles.links}>
+          About Us<div className={styles.underline}></div>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
